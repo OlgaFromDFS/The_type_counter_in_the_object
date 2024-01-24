589af1286f4dddeff84dd51f8c2812bee2ed2325
Olga <oluaribalka@yandex.ru>
2024-01-24 12:04:02 +0300

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# The_type_counter_in_the_object
+# the-type-counter-in-the-object-node-js
+
+Node.js module to create a new object in which the keys are types, the values are the number of these types.
+
+## Installation
+
++ ```npm install the-type-counter-in-the-object-node-js```
+
+## Usage
+
+<script>
+
+  /*
+      Import the module.
+  */
+  const theTypeCounterInTheObject = require('./the_type_counter_in_the_object.js');
+
+  /*
+      Use the module according to the instructions.
+      Create a variable. Write to it, through a dot from the "module", a function (createObjectOfType(object)) with an object argument that creates a new object with types and their number.
+  */
+
+  const result = theTypeCounterInTheObject.createObjectOfType({ field: undefined, anotherField: [], yetAnother: [undefined] });
+
+  /*
+      Output/use the result, for example, to the console.
+  */
+
+  console.log(result);
+
+</script>
+
+## License
+
+MIT
---
+++ the-type-counter-in-the-object-node-js.js
@@ -0,0 +1,31 @@
+module.exports.createObjectOfType = createObjectOfType;
+
+/**
+ *  Returns a new object. The types of values from the argument object will be written to the keys, and the number of these types will be written to the values.
+ *  @param {object} object A simple, non-nested object.
+ *  @returns {object} An object with keys-types and values-the number of these types.
+ */
+function createObjectOfType(object) {
+    const values = Object.values(object);
+    const newObjectFromTypeAndQuantity = {};
+
+    for (const value of values) {
+        let type = typeof value;
+
+        if (value === null) {
+            type = 'null';
+        }
+
+        if (Array.isArray(value)) {
+            type = 'array';
+        }
+
+        if (newObjectFromTypeAndQuantity[type]) {
+            newObjectFromTypeAndQuantity[type] += 1;
+        } else {
+            newObjectFromTypeAndQuantity[type] = 1;
+        }
+    }
+    
+    return newObjectFromTypeAndQuantity;
+};
